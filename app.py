d6c430eb2d3a2632c0a418cb2898472c151ca959
Rob Fowler <rtf04@outlook.com>
2022-08-21 18:09:36 -0400

    refectored with new folder setup

---
+++ app.py
@@ -0,0 +1,9 @@
+from lib import *
+
+vault_folder = r"D:/Rob/InfoCenter"
+moc_folder_rel = r"/Topics/Abortion"
+moc_folder_abs = Path(vault_folder,moc_folder_rel)
+
+
+
+
--- lib.py
+++ lib.py
@@ -1,20 +1,11 @@
 from pathlib import Path
 
-vault = Path(r"D:/Rob/InfoCenter")
-moc_folder = r"Topics/United_States"
 
-
-def moc(moc_folder):
-    '''
-    vault_folder : a Path object.  The complete path to the Obsidan vault folder.
-        Example: Path(r"D:/Rob/InfoCenter")
-
-    moc_folder : the path of the folder whose content is to be mapped, relative to
-    vault_folder.
-        Example: r"Topics/United_States"
-
-
+def map_content(moc_folder):
+    '''   
     Create a Map of Content (MOC) of all files in the selected folder.
+
+    moc_folder : complete path to the folder of files to be mapped.
 
     Each document will have a corresponding Markdown file, with information about the file, such as 
     text excepts, source URL,thoughts and response, why it's important, etc.  It's basically
@@ -31,36 +22,38 @@ def moc(moc_folder):
         - Resides in same folder as the topic files, and the corresponding Markdown files.
         - Links to all the "metadata" Markdown files.
 
-    '''
-    # Absolute path to MOC folder
-    vault_moc = Path(vault, moc_folder)
+'''  
 
-    for item in vault_moc.iterdir():
+    for item in moc_folder.iterdir():
+        # Create "metadata" Markdown pages for each item.
+        # Existing Markdown files are kept as-is. Meta files are only for
+        # non Markdown docs.
         if item.is_file() and item.suffix != "md":
-            meta_file_path = Path(vault_moc,item.stem + ".md")
+            meta_file_path = Path(moc_folder,item.stem + ".md")
             meta_file_title = item.stem.replace("_"," ")
-            meta_file_link_path = "[[" + moc_folder + "/" + item.name + "|" + meta_file_title + "]]"
+            
+            # The link to be created within the metadata file that will point to the item.         
+            meta_file_link_path = "[[" + str(moc_folder) + "/" + item.name + "|" + meta_file_title + "]]"
 
             with open(meta_file_path,"w",encoding="utf-8") as meta_file:
                 meta_file.write("# " + meta_file_title + "\n\n")
                 meta_file.write(meta_file_link_path + "\n")    
 
-            print(str(meta_file_title))
             print(str(meta_file_path))
+
         elif item.is_dir():
-            moc(item)
+            print(f"{str(item)} is a folder.")
+
 
             
     # Create an index page, with links to all the metadata pages.
     # Page will have the same name as moc_folder, plus ".md"
-    moc_index_file_path = Path(vault_moc, "_MOC_Index_" + vault_moc.name + ".md")
-    moc_index_file_title = "# " + vault_moc.name.replace("_"," ") + "\n\n"
+    moc_index_file_path = Path(moc_folder, "_MOC_Index_" + moc_folder.name + ".md")
+    moc_index_file_title = "# " + moc_folder.name.replace("_"," ") + "\n\n"
     with open(moc_index_file_path,"w",encoding = 'utf-8') as index_file:
         index_file.write(moc_index_file_title)    
-        for item in vault_moc.iterdir():
+        for item in moc_folder.iterdir():
             if item.suffix == ".md" and item != moc_index_file_path:
-                link_path = "[[" + moc_folder + "/" + item.stem + ".md" + "|" + item.stem.replace("_"," ") + "]]\n"
+                link_path = "[[" + str(moc_folder) + "/" + item.stem + ".md" + "|" + item.stem.replace("_"," ") + "]]\n"
                 index_file.write(link_path)
-
-
 
