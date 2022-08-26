d9e6da7fd82ad95bbbdc61e46ad8665159536482
Rob Fowler <rtf04@outlook.com>
2022-08-25 22:30:28 -0400

    added a class for a vault file with an ID number.

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
+from cgi import test
 from lib import *
 
-vault_folder = r"D:/Rob/InfoCenter"
-moc_folder_rel = r"Church_Journal"
-# document_folder = r"D:\Rob\The_Compendium\Documents"
-# document_folder = r"C:\Users\robf.PCS\Downloads\The_Compendium\Documents"
-document_folder = r"D:\Rob\Downloads\Compendium\Documents"
+vault_folder = r"D:\Rob\Compendium"
+
+test_file = r"D:\Rob\Compendium\06_Library\06.08.005_2002_10_17_Speed_and_Skill"
+
+my_file = VaultFile(test_file)
+
+print(my_file.file_id)
+print(my_file.category)
+print(my_file.topic_num)
+print(my_file.counter)
+print(my_file.topic_id)
+print(my_file.next_topic_id)
+print(my_file.next_counter)
 
 
-my_assistant = FilingAssistant(document_folder)
-my_assistant.process_files()
+
+
+
+
+
+
+
+
 
 
 
