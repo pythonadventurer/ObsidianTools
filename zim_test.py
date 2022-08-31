dec3ef438a82d9c4a1b94f7aa937092e87c5c3be
Rob Fowler <robtf04@outlook.com>
2022-08-31 17:19:15 -0400

    updated zim to md

--- zim_test.py
+++ zim_test.py
@@ -3,15 +3,9 @@ from pathlib import Path
 from lib import *
 
 
-zim_header = "====== 2022-02-14 Replace LogAppend with SendToLog ======"
+zim_file = Path(r"C:\Users\robf.PCS\Documents\Notebook\Home\Projects\2022\2022-01-05_Eliminate_DupTest_Field.txt")
+dest_dir = config.converted_projects
 
-dest_dir = Path(r"C:\Users\robf.PCS\Documents\converted_projects")
-# for item in config.projects.iterdir():
-#     if item.is_file():
-#         zim_to_md(item, dest_dir)
-
-my_vault = ObsidianVault(config.vault_folder)
-my_vault.remove_file_ids(my_vault.vault_folder)
-
-
-
+for item in config.projects.iterdir():
+    if item.is_file():
+        zim_to_md(item,dest_dir)
