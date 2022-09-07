d5229e29cdcaa9840ac08403dad05de2938005d9
Rob Fowler <rtf04@outlook.com>
2022-09-07 07:19:14 -0400

    updated fix tiles to use ObsidianNote class

--- fix_titles.py
+++ fix_titles.py
@@ -4,4 +4,10 @@ from pathlib import Path
 
 
 for file in resource_catalog.iterdir():
-    fix_titles(file)
+    file_obj = ObsidianNote(file)
+    if file_obj.title != file.stem:
+        file_obj.set_title(file.stem)
+        file_obj.save()
+        print("Fixed: " + file.name)
+            
+
