c5fff7a54ca005cc0e86209ca6da215b40812656
Rob Fowler <robtf04@outlook.com>
2022-09-27 17:57:43 -0400

    worked on zim code

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ open_task = "- [ ] #task 📅 "
 vault = Path(r"D:\Rob\my_vault")
 diarium = Path(r"D:\OneDrive\Rob\_INBOX\_TO_BE_FILED\Diarium")
 journal_prep = Path(r"D:\Rob\my_vault\Lifeline\prep")
+zim_dir = Path(r"C:\Users\Robf.DESKTOP-JNCQ9MB\Documents\Notebook\Journal")
 
 
 
