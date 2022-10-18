27e8a4ae4d8a24b3f8d38312c57927ec5ef16e54
Rob Fowler <robtf04@outlook.com>
2022-10-18 17:31:43 -0400

    added docs backup

--- config.py
+++ config.py
@@ -1,11 +1,21 @@
 import os
 from pathlib import Path
 
+python_dir_w = Path(r"C:\Users\Robf.DESKTOP-JNCQ9MB\AppData\Local\Programs\Python\Python310\Python.exe")
+
+project_dir_w = Path(r"C:\Users\Robf.DESKTOP-JNCQ9MB\Documents\ObsidianTools")
+
+
+
 completed_task = "- [x] #task 📅 "
 open_task = "- [ ] #task 📅 "
 
 vault = Path(r"C:\Users\Robf.DESKTOP-JNCQ9MB\Documents\DevNotes2")
-# vault = Path(r"F:\DevNotes2")
 vault_backup = Path(r"F:\devnotes")
 backend = Path(r"\\server\Access\Backend_v2")
 backend_backup = Path(r"F:\backend")
+docs = Path(r"C:\Users\Robf.DESKTOP-JNCQ9MB\Documents")
+docs_bkp = Path(r"F:\documents")
+
+
+
