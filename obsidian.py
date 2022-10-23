a448ca9495b71d9704dc78e0e26d15258c3a983f
Rob Fowler <rtf04@outlook.com>
2022-10-23 16:43:11 -0400

    more refactoring of ObsidianNote

--- obsidian.py
+++ obsidian.py
@@ -6,27 +6,6 @@ import os
 
 
 """
-file does not exist: create new file and assign it a file id in metadata.
-file exists:
-    - has metadata
-        - read into a list
-    - does not have metadata
-        - add metadata with file id.
-
-Note blocks:
-    self.all_text : the entire text contents of the note
-    self.metadata_text : the metadata block at the top
-    self.body_text : the body of the text that comes after self.metadata_text
-
-
-
-Refactor:
-- self.read_metadata : read metadata from the file into a list format
-- self.write_metadata : create metadata text block from the list
-- self.write_file : write the contents of self.metadata and self.body_text to the file
-- self.add_file_id : add a file id based on creation date
-    - If no existing metadata, create it.
-    - If existing metadata, append.
 
 """
 
@@ -43,51 +22,41 @@ class ObsidianNote:
                 ctime = os.path.getctime(file_path)
                 self.file_id = dt.fromtimestamp(ctime).strftime("%Y.%m.%d.%H.%M.%S.%f")[:23]
  
-                # create the metadata list
-                self.metadata = [["file_id",[self.file_id]]]
-                
-                # body text is blank for new notes
-                self.body_text = ""
-
-                new_note.write(self.write_metadata(self.metadata))
-
         else:
             with open(file_path,"r",encoding="utf-8") as new_note:
+                ctime = os.path.getctime(file_path)
+                self.file_id = dt.fromtimestamp(ctime).strftime("%Y.%m.%d.%H.%M.%S.%f")[:23]
                 self.all_text = new_note.read()
 
-            ctime = os.path.getctime(file_path)
-            self.file_id = dt.fromtimestamp(ctime).strftime("%Y.%m.%d.%H.%M.%S.%f")[:23]  
-            
-            # if the file has no metadata, add file id.
-            if self.all_text.startswith("---") == False:    
-                self.metadata = [["file_id",[self.file_id]]]
-                 
-                # since the existing text has no metadata block, body_text = all_text.
-                self.write_file(self.write_metadata(self.metadata),self.all_text)
-                
+            self.metadata = self.read_metadata()
+            if self.metadata != []:
+                self.body_text = self.all_text[len(self.write_metadata(self.metadata)):]
+
             else:
-                self.metadata = self.read_metadata(self.all_text)
-                self.body_text = self.all_text[len(self.metadata_text):]
+                self.body_text = self.all_text
                 
 
-    def read_metadata(self,text):
+    def read_metadata(self):
         """
         read metadata from text into a list.
         """
-        metadata_start = text.find("---\n") + 4
-        metadata_end = text.find("---\n",metadata_start)
-        metadata_contents = text[metadata_start:metadata_end].split("\n")[:-1]
-        for line in range(len(metadata_contents)):
-            metadata_contents[line] = metadata_contents[line].split(": ")
-            metadata_contents[line][1] = metadata_contents[line][1].split(" ")
+        if self.all_text.find("---\n") == -1:
+            return []
 
-        return metadata_contents
+        else:
+            metadata_start = self.all_text.find("---\n") + 4
+            metadata_end = self.all_text.find("---\n",metadata_start)
+            metadata_contents = self.all_text[metadata_start:metadata_end].split("\n")[:-1]
+            for line in range(len(metadata_contents)):
+                metadata_contents[line] = metadata_contents[line].split(": ")
+                metadata_contents[line][1] = metadata_contents[line][1].split(" ")
+
+            return metadata_contents
    
     def write_metadata(self, metadata):
         """
         User makes changes to the metadata by altering the metadata list.  This list is then written to the metadata text.
 
-        Writing the metadata from the list overwrites the existing metadata text.
         """
         new_metadata = "---\n"
 
@@ -103,105 +72,123 @@ class ObsidianNote:
         return new_metadata
 
 
-    def add_metadata(self, metadata, key, value):
+    def add_metadata(self, key, value):
         """
         If key exists, append the value to its list.
         If key does not exist, add it and the value.
         """
         key_exists = False
-        for n in range(len(metadata)):
-            if metadata[n][0] == key:
+        for n in range(len(self.metadata)):
+            if self.metadata[n][0] == key:
                 key_exists = True
-                metadata[n][1].append(value)
+                if type(value) == list:
+                    self.metadata[n][1].extend(value)
+                else: 
+                    self.metadata[n][1].append(value)
     
         if key_exists == False:
             if type(value) == list:
-                metadata.append([key, value])
+                self.metadata.append([key, value])
             else:
-                metadata.append([key,[value]])
-
-        return metadata
-
-
-    def remove_metadata(self, metadata, key, value):
-        # return metadata unchanged if key doesn't exist.
-        for n in range(len(metadata)):
-            if metadata[n][0] == key:  
-                if type(metadata[n][1]) == list:
-                    metadata[n][1].remove(value)
-                else:
-                    metadata[n][1] = metadata[n][1].replace(value + " ","")
-
-            return metadata
+                self.metadata.append([key,[value]])
+            
+        self.save_file()
 
 
-    def remove_key(self, metadata, key):
-        for n in range(len(metadata)):
-            if metadata[n][0] == key:
-                metadata.remove(metadata[n])
-                return metadata
+    def add_file_id(self):
+        self.add_metadata("file_id",self.file_id)
 
 
-    def remove_heading(self, level, text):
+    def remove_metadata_value(self, key,value):
+        # remove the selected value from a metadata item
+        for n in range(len(self.metadata)):
+            if self.metadata[n][0] == key:  
+               self.metadata[n][1].remove(value)
+               self.save_file()
+
+    def remove_metadata_key(self, key):
+        for n in range(len(self.metadata)):
+            if self.metadata[n][0] == key:
+                self.metadata.remove(self.metadata[n])
+                self.save_file()
+
+    def convert_tags_line(self):
+        """
+        Converts tags from a line in the text of the note
+        to tags in metadata. Overwrites any existing tags in
+        metadata.
+        """
+        for line in self.body_text.split("\n"):
+            if line.startswith("Tags: "):
+                tags_line = line
+                new_tags_line = tags_line.replace("Tags: ","")
+                new_tags_line = new_tags_line.replace("#","")
+                # all that's left now is tags separated by spaces.
+                new_tags_line = new_tags_line.split(" ")
+                self.remove_metadata_key("tags")
+                self.add_metadata("tags", new_tags_line)
+                self.remove_line("Tags: ")
+                self.save_file()
+
+
+    def remove_heading(self, level):
         """
         Remove the first heading encountered in the text that corresponds to the selected level.
         """
         heading_text = "#" * level + " "
-        heading = text[text.find(heading_text):text.find("\n",text.find(heading_text))]
-        text = text.replace(heading + "\n", "")
-        return text
+        heading = self.body_text[self.body_text.find(heading_text):self.body_text.find("\n",self.body_text.find(heading_text))]
+        if heading != "":
+            self.body_text = self.body_text.replace(heading + "\n", "")
+            self.save_file()
 
-    def add_heading(self, level, heading_text, text):
-        heading_text = "#" * level + " " + heading_text
-        text = text + heading_text + "\n"
-        return text
-
-
-    def add_paragraph(self, paragraph, text):
-        """
-        Add a paragraph to the text and return the text.
-        """
-        text = text + paragraph + "\n\n"
-        return text
-
-    def add_bullet_list(self, list, text):
-        for item in list:
-            text += "- " + item + "\n"
-        text += "\n"
-        return text
-
-    def remove_line(self,start_text, text):
+    def remove_line(self,start_text):
         """
         Remove a line within the text that starts with start_text.
         Return the text of the removed line.
         """
-        for line in text.split("\n"):
+        for line in self.body_text.split("\n"):
             if line.startswith(start_text):
-                line_text = line
-        
-        text = text.replace(line_text + "\n","")
-        return text, line_text
+                line_text = line 
+                self.body_text = self.body_text.replace(line_text + "\n","")
+                self.save_file()
+
+    def add_heading(self, level, heading_text):
+        heading_text = "#" * level + " " + heading_text
+        self.body_text = self.body_text + heading_text + "\n"
+        self.save_file()
+
+    def add_title(self, title_text):
+        # Add a level 1 title to the top of the file
+        title_text = "# " + title_text + "\n"
+        self.body_text = title_text + self.body_text
+        self.save_file()      
 
 
-    def convert_tags_line(self, metadata, text):
-        """
-        Converts tags from a line in the text of the note
-        to tags in metadata. Overwrites existing tags in
-        metadata.
-        """
-        output = self.remove_line("Tags: ",text)
+
+    # def add_paragraph(self, paragraph, text):
+    #     """
+    #     Add a paragraph to the text and return the text.
+    #     """
+    #     text = text + paragraph + "\n\n"
+    #     return text
+
+    # def add_bullet_list(self, list, text):
+    #     for item in list:
+    #         text += "- " + item + "\n"
+    #     text += "\n"
+    #     return text
+
+
+
+
+ 
         
-        new_text = output[0]
-        tags_line = output[1]
-        tags_line = tags_line.replace("Tags: ","")
-        tags_line = tags_line.replace("#","")
-        tags_line = tags_line.split(" ")
-        return add_metadata("tags", tags_line)
-        
-    def write_file(self,metadata_text,body_text):
+    def save_file(self):
         with open(self.file_path,"w",encoding="utf-8") as f:
-            f.write(metadata_text)
-            f.write(body_text)
+            if self.metadata != []:
+                f.write(self.write_metadata(self.metadata))
+            f.write(self.body_text)
+
 
             
 
--- test_obsidian2.py
+++ test_obsidian2.py
@@ -1,26 +1,33 @@
 from pathlib import Path
 from obsidian import *
 
-new_note = Path(r"C:\Users\Robf.DESKTOP-JNCQ9MB\Documents\DevNotes2\test_note.md")
+new_note = Path(vault,"Vault_Inbox/Test Note.md")
+existing_note = Path(vault, "Vault_Inbox/Hitler on Christianity and the Nation.md")
+old_note = Path(vault, "Vault_Inbox/Choosing the Right Container.md")
+
 
 my_note = ObsidianNote(new_note)
+my_existing_note = ObsidianNote(existing_note)
+my_old_note = ObsidianNote(old_note)
+
+# print(my_note.metadata) 
+# print(my_existing_note.metadata)
+# print(my_existing_note.body_text)
+
+# my_note.add_file_id()
+
+# my_note.add_metadata("tags", ["frogs","snails"])
+# my_note.remove_line("This is the barn")
+
+my_old_note.convert_tags_line()
 
 
 
 
 
 
+# my_note.remove_metadata_value("tags", "frogs")
 
-
-
-
-
-# my_note.add_heading("Ice Cream List", 2)
-# my_note.add_bullet_list(["Vanilla","Rocky Road","Strawberry"])
-# my_note.add_heading("Pizza List", 2)
-# my_note.add_bullet_list(["Plain Cheese","Pepperoni","Anchovy","Sausage"])
-
-# my_note.convert_tags_line()
-
+# my_note.remove_metadata_key("tags")
 
 
