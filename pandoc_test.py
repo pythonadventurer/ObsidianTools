0d847461e04b6430c636841e6af3d0b39083245c
Rob Fowler <robtf04@outlook.com>
2022-11-02 17:58:22 -0400

    pandoc links

--- pandoc_test.py
+++ pandoc_test.py
@@ -1,17 +1,25 @@
 from config import *
 import pypandoc
 from pathlib import Path
+import re
 
-input_dir = Path(vault,r"Databases\DataManager2\User Documentation")
-output_dir = Path(vault,r"Databases\DataManager2\User Documentation\HTML")
+link_search = re.compile("\((.+\.md)\)")
+switchboard = Path(r"C:\Users\Robf.DESKTOP-JNCQ9MB\Documents\DevNotes2\Databases\DataManager2\User Documentation\Main Switchboard.md")
+
+with open(switchboard,"r") as file:
+    text = file.read()
 
 
-for item in input_dir.iterdir():
-    if item.is_file() and item.suffix == ".md":
-        input_file = Path(input_dir,item.name)
-        output_file = Path(output_dir,item.stem + ".html")
-        title = "--metadata-title=" + input_file.stem.replace("_"," ")
+# input_dir = Path(vault,r"Databases\DataManager2\User Documentation")
+# output_dir = Path(vault,r"Databases\DataManager2\User Documentation\HTML")
 
-        output = pypandoc.convert_file(input_file, "html",outputfile=output_file,extra_args=['-s','-c styles/pandoc.css'])
-        print("Converted: " + item.name)
+
+# for item in input_dir.iterdir():
+#     if item.is_file() and item.suffix == ".md":
+#         input_file = Path(input_dir,item.name)
+#         output_file = Path(output_dir,item.stem + ".html")
+#         title = "--metadata-title=" + input_file.stem.replace("_"," ")
+
+#         output = pypandoc.convert_file(input_file, "html",outputfile=output_file,extra_args=['-s','-c styles/pandoc.css'])
+#         print("Converted: " + item.name)
 
