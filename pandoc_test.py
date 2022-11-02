3721c60f71ef7c3c25b665fa7b1c5912ccaeea61
Rob Fowler <robtf04@outlook.com>
2022-11-02 17:47:32 -0400

    added css to pandoc

--- pandoc_test.py
+++ pandoc_test.py
@@ -2,9 +2,16 @@ from config import *
 import pypandoc
 from pathlib import Path
 
-input_file = Path(vault,r"Databases\DataManager2\User Documentation\Main Switchboard.md")
-output_file = Path(vault,r"Databases\DataManager2\User Documentation\Main_Switchboard.html")
+input_dir = Path(vault,r"Databases\DataManager2\User Documentation")
+output_dir = Path(vault,r"Databases\DataManager2\User Documentation\HTML")
 
 
-output = pypandoc.convert_file(input_file, "html",outputfile=output_file)
+for item in input_dir.iterdir():
+    if item.is_file() and item.suffix == ".md":
+        input_file = Path(input_dir,item.name)
+        output_file = Path(output_dir,item.stem + ".html")
+        title = "--metadata-title=" + input_file.stem.replace("_"," ")
+
+        output = pypandoc.convert_file(input_file, "html",outputfile=output_file,extra_args=['-s','-c styles/pandoc.css'])
+        print("Converted: " + item.name)
 
--- test.bat
+++
@@ -1,3 +0,0 @@
-pandoc -s --metadata title="Main Switchboard" -o "C:\Users\Robf.DESKTOP-JNCQ9MB\Documents\DevNotes2\Databases\DataManager2\User Documentation\HTML\Main_Switchboard.html" "C:\Users\Robf.DESKTOP-JNCQ9MB\Documents\DevNotes2\Databases\DataManager2\User Documentation\Main Switchboard.md"
-
-
