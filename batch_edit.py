930832920dfed7776f29df839e313075a1584d6e
Rob Fowler <rtf04@outlook.com>
2022-11-03 14:29:44 -0400

    batch editing vault files

--- batch_edit.py
+++ batch_edit.py
@@ -1,17 +1,28 @@
 from pathlib import Path
 from obsidian import *
 
-folder = Path(r"C:\Users\Robf.DESKTOP-JNCQ9MB\Documents\DevNotes2\Projects\2020")
+folder = Path(r"D:\Rob\Vault\Exercise_Log")
 
-for file in folder.iterdir():
-    note = ObsidianNote(file)
-    note.replace_text("Specimen_#_Field1","Specimen # Field1")
-    
-    # note.add_file_id()    
-    # note.remove_heading(1)
-    # note.remove_metadata_key("tags")
-    # note.add_metadata("tags", "Projects/2022")   
-    # note.remove_line("//Updated:")
-    # note.remove_line("//Created")
-    # note.remove_line("Created")
-    print(f"Processed file: {file.name}")
+def fix_log(folder_name):
+    for item in folder_name.iterdir():
+        if item.is_file():
+            if item.suffix == ".md":
+                note = ObsidianNote(item)
+
+            # note.add_file_id()    
+            # note.convert_tags_line()
+
+            # note.remove_heading(1)
+            # # # # note.remove_metadata_key("tags")
+            # # # # note.add_metadata("tags", "Projects/2022")   
+            # # # # note.remove_line("//Updated:")
+            # # # # note.remove_line("//Created")
+            note.remove_line("Created")
+            note.replace_text("./","")
+
+            print(f"Processed file: {item.name}")
+        else:
+            if item.is_dir():
+                fix_log(item)
+
+fix_log(folder)
